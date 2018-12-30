86d9c601b924c7ce407e137216f65f8d6f4b640d
Ing. Aldair Morales Cuellar <al-dair12@hotmail.es>
2018-12-30 14:19:44 -0500

    modulos pages

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import {NosotrosPageModule} from '../pages/nosotros/nosotros.module'; //Importación de la Página
+
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -21,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NosotrosPageModule //Página de Nosotros
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- App/src/pages/about/about.ts
+++ App/src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {NosotrosPage} from '../nosotros/nosotros';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -9,6 +9,10 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goPageNosotros():void{
+    this.navCtrl.push(NosotrosPage);
   }
 
 }
