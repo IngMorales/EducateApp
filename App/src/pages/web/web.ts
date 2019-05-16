80b933e86b40180b155e70b04d2d7726b6d4f422
Ing. Aldair Morales Cuellar <al-dair12@hotmail.es>
2019-05-15 23:43:08 -0500

    url hidde

--- App/src/pages/web/web.ts
+++ App/src/pages/web/web.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the WebPage page.
@@ -23,8 +23,32 @@ export class WebPage {
     console.log('ionViewDidLoad WebPage');
   }
 
-  openLink(){
-    this.iap.create("https://www.asesoriaseducate.com/");
+  /*openLink(){
+    this.iap.create("https://www.asesoriaseducate.com/", "location=no, toolbar=no");
+    
+    //var ref = cordova.InAppBrowser('http://apache.org', '_blank', 'location=no');
+  }*/
+  
+/*
+  openLink(url: string) {
+    const options: InAppBrowserOptions = {
+      zoom: 'no',
+      location: 'no',
+      toolbar: 'no'
+    };
+    const browser = this.iap.create('https://www.asesoriaseducate.com/','_self','toolbar=no');
+  }
+*/
+
+  private openLink(url: string, title?: string) {
+    let options: InAppBrowserOptions = {
+      toolbarcolor: "#488aff",
+      hideurlbar: "yes",
+      closebuttoncolor: "#fff",
+      navigationbuttoncolor: "#fff"
+    };
+    const browser = this.iap.create('https://www.asesoriaseducate.com/', "_blank", options);
   }
 
+  //this.inAppBrowser.create('http://sco7.com/filemanager/sapphire/','_self',{ toolbar: 'no'});
 }
